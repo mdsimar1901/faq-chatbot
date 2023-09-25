c042c076c04e7456ee388ad1f8417900141af7c6
mdsimar1901 <66200713+mdsimar1901@users.noreply.github.com>
2023-09-25 09:47:18 +0530

    Added tailwindcss

---
+++ src/Components/MessageView.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface MessageViewProps {
+  text: string;
+}
+
+const MessageView: React.FC<MessageViewProps> = ({ text }) => {
+  return (
+    <div className="text-white text-sm w-48 mb-2 p-2 rounded-lg self-start text-left break-words whitespace-pre-wrap bg-purple-700">
+      <div className="flex gap-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 pink-blue-700"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+        <div className="bg-pink-700 text-white p-2 rounded-lg">{text}</div>
+      </div>
+    </div>
+  );
+};
+
+export default MessageView;
