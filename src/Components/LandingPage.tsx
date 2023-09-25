61e1c67b7bce6a16059b006f0966294833749992
mdsimar1901 <66200713+mdsimar1901@users.noreply.github.com>
2023-09-25 15:56:02 +0530

    Created login form no connection made

---
+++ src/Components/LandingPage.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Slide,
+  Typography,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import Chatbot from "./Chatbot";
+import ChatIcon from "@mui/icons-material/Chat";
+
+const LandingPage: React.FC = () => {
+  const [isBotActivated, setIsBotActivated] = useState<boolean>(false);
+
+  const toggleChatbot = (): void => {
+    setIsBotActivated((prev) => !prev);
+  };
+
+  return (
+    <Container sx={styles.container}>
+      <Typography variant="h1" sx={styles.title}>
+        AI Powered ChatBot 🚀
+      </Typography>
+      <Typography variant="h6" sx={styles.description}>
+        Click on the below 🗨️icon to continue.
+      </Typography>
+      
+      <a href="https://github.com/" rel="noopener">GitHub Link</a>
+      <a href="" rel="noopener">Link for Documentation 📄</a>
+      <Slide
+        direction={isBotActivated ? "up" : "down"}
+        in={isBotActivated}
+        mountOnEnter
+        unmountOnExit
+        {...(isBotActivated ? { timeout: 1000 } : {})}
+      >
+        <Box sx={styles.chatbotContainer}>
+          {isBotActivated && <Chatbot />}
+        </Box>
+      </Slide>
+
+      <Button
+        variant="contained"
+        onClick={toggleChatbot}
+        sx={styles.button}
+      >
+        {isBotActivated ? <CloseIcon /> : <ChatIcon />}
+      </Button>
+    </Container>
+  );
+}
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    minWidth: "100%",
+    height: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff", // Updated to silky white
+    overflow: "hidden",
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: "20px",
+    color: "#333", 
+    fontWeight: "600",
+    fontSize: { xs: "28px", sm: "32px", lg: "46px" },
+  },
+  description: {
+    textAlign: "center",
+    marginBottom: "20px",
+    width: { sm: "80%", lg: "50%" },
+    fontWeight: "normal",
+    fontSize: { xs: "16px", sm: "20px", lg: "20px" },
+    color: "#555", 
+  },
+  chatbotContainer: {
+    position: "absolute",
+    right: "50px",
+    bottom: { lg: "85px" },
+  },
+  button: {
+    height: "40px",
+    width: "50px",
+    borderRadius: "40px",
+    position: "fixed",
+    bottom: { xs: "15px", lg: "16px" },
+    right: "25px",
+    backgroundColor: "#333",
+    ":hover": {
+      backgroundColor: "#555", 
+    },
+  },
+};
+
+export default LandingPage;
---
+++ src/Components/Login.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+function LoginPage(): JSX.Element {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+
+  const handleLogin = () => {
+    console.log(username);
+    console.log(password);
+    
+  };
+
+  return (
+    <Container sx={styles.container}>
+      <Typography variant="h2" sx={styles.title}>
+        User Login
+      </Typography>
+      <Box sx={styles.formContainer}>
+        <TextField
+          label="Username"
+          variant="outlined"
+          fullWidth
+          value={username}
+          onChange={handleUsernameChange}
+          sx={styles.input}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          fullWidth
+          value={password}
+          onChange={handlePasswordChange}
+          sx={styles.input}
+        />
+        <Button
+          variant="contained"
+          onClick={handleLogin}
+          sx={styles.button}
+        >
+          Log In
+        </Button>
+      </Box>
+    </Container>
+  );
+}
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    minWidth: "100%",
+    height: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff", 
+    overflow: "hidden",
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: "20px",
+    color: "#333",
+    fontWeight: "600",
+    fontSize: { xs: "28px", sm: "32px", lg: "46px" },
+  },
+  formContainer: {
+    width: { xs: "90%", sm: "70%", md: "50%" },
+    backgroundColor: "#f0f0f0",
+    padding: "20px",
+    borderRadius: "10px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+  },
+  input: {
+    marginBottom: "20px",
+  },
+  button: {
+    backgroundColor: "#333",
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: "#555",
+    },
+  },
+};
+
+export default LoginPage;
